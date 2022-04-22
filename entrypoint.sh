cd37c79e532d0e4c92ccfebf8ca17dea6b4be276
Gabriel Ferreira <gabriel.oliveira@platformbuilders.io>
2022-04-22 14:02:48 -0300

    Changes in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,15 +9,20 @@ if [[ "$1" == "before-destroy" ]]; then
     git config --local user.name "GitHub Action"
     echo "$2/$3/$4/$5" >> destroy_path.txt
 
-    printf "\033[0;32m============> Git push: Branch tf_destroy_$ENVIRONMENT_$RESOURCE \033[0m\n"
+    printf "\033[0;32m============> Publish .env file \033[0m\n"
+    echo "GITOPS_BRANCH=homolog" >> $GITHUB_ENV
+    echo "ENVIRONMENT=$2" > destroy_env_vars.env
+    echo "REGION=$3" > destroy_env_vars.env
+    echo "SERVICE=$4" > destroy_env_vars.env
+    echo "RESOURCE=$5" > destroy_env_vars.env
+
+    printf "\033[0;32m============> Git push: Branch tf_destroy_$2_$5 \033[0m\n"
     git add -A .
     git commit -m "Destroy resource $5 in $2"
     git push --set-upstream origin "tf_destroy_$2_$5"
 
-
     printf "\033[0;32m============> Open PR: tf_destroy_$2_$5 -> develop \033[0m\n"
     gh pr create --head release --base develop -t "[ Terraform destroy ]: Automatic PR opened by GitHub Actions"
-
 
 elif [[ "$1" == "after-destroy" ]]; then
 
